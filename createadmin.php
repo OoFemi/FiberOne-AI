c8eab3ec9208eb650c74fbcaa9afa5472e18380d
oofemi <femifeel12@gmail.com>
2026-07-08 23:07:17 +0000

    adminpage

---
+++ createadmin.php
@@ -0,0 +1,33 @@
+<?php
+
+require "db.php";
+
+$username = "admin";
+
+$passwordHash =
+password_hash(
+    "Atlas123!",
+    PASSWORD_DEFAULT
+);
+
+$role = "admin";
+
+$stmt =
+$conn->prepare(
+"INSERT INTO users
+(username,password_hash,role)
+VALUES (?,?,?)"
+);
+
+$stmt->bind_param(
+"sss",
+$username,
+$passwordHash,
+$role
+);
+
+$stmt->execute();
+
+echo "Admin Created";
+
+?>
